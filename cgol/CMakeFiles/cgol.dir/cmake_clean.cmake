53f2432326325f91e3daf7deab3c05c68ddc58ca
Ryan Jeffrey <pwishie@gmail.com>
2019-03-13 18:59:10 -0700

    deletions

---
+++ cgol/CMakeFiles/cgol.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cgol.dir/src/argh.cc.o"
+  "CMakeFiles/cgol.dir/src/simulation.cc.o"
+  "bin/cgol.pdb"
+  "bin/cgol"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cgol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
