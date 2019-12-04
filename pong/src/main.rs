328c87aa0d124b54ca316b09b165245ecbe7a633
rmj <pwishie@gmail.com>
2019-12-04 00:29:52 -0800

    pong frame rate control

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -4,38 +4,43 @@ use sdl2::rect::Rect;
 use std::error::Error;
 use std::time::{Duration, Instant};
 
-enum paddle_direction {
+enum PaddleDuration {
     Up,
     Down,
 }
 
 const PADDLE_MOV: i32 = 20;
 
-struct pong_data {
+struct PongData {
     player1: Rect,
     player2: Rect,
 
     ball: Rect,
+
+    player1_score: u32,
+    player2_score: u32,
 }
 
-impl pong_data {
+impl PongData {
     // initialize default state for the game
-    fn new() -> pong_data {
-        pong_data {
+    fn new() -> PongData {
+        PongData {
             player1: Rect::new(20, 250, 20, 100),
             player2: Rect::new(850, 250, 20, 100),
-            ball: Rect::new(450, 300, 20, 20),
+            ball: Rect::new(450, 300, 10, 10),
+            player1_score: 0,
+            player2_score: 0,
         }
     }
 
     // makes sure the rect is in bounds.
     fn correct_position(rect: Rect) -> Rect {
         let y: i32;
-        
-        if rect.y >= (900 - 100) {
-            y = 900 - 100;
-        } else if rect.y <= (0 + 100) {
-            y = 100;
+
+        if rect.y <= 0 {
+            y = 0;
+        } else if rect.y >= 800 {
+            y = 800;
         } else {
             y = rect.y;
         }
@@ -44,21 +49,21 @@ impl pong_data {
     }
 
     // moves the player
-    fn move_player(&mut self, dir: paddle_direction) {
+    fn move_player(&mut self, dir: PaddleDuration) {
         match dir {
-            paddle_direction::Up => self.player1.y += PADDLE_MOV,
-            paddle_direction::Down => self.player1.y -= PADDLE_MOV,
+            PaddleDuration::Up => self.player1.y -= PADDLE_MOV,
+            PaddleDuration::Down => self.player1.y += PADDLE_MOV,
         }
 
-        self.player1 = pong_data::correct_position(self.player1);
+        self.player1 = PongData::correct_position(self.player1);
     }
 
     // runs the game simulation
     fn simulate(&mut self) {}
 }
 
-struct sdl_data {
-    // both of these are in the sdl_data so they don't get destroyed until the game ends
+struct SDLData {
+    // both of these are in the SDLData  so they don't get destroyed until the game ends
     context: sdl2::Sdl,
     video_subsystem: sdl2::VideoSubsystem,
 
@@ -67,8 +72,8 @@ struct sdl_data {
     event_pump: sdl2::EventPump,
 }
 
-impl sdl_data {
-    fn new() -> Result<sdl_data, Box<dyn Error>> {
+impl SDLData  {
+    fn new() -> Result<SDLData , Box<dyn Error>> {
         let new_context = sdl2::init()?;
         let new_vsubsystem = new_context.video()?;
         let new_window = new_vsubsystem
@@ -79,7 +84,7 @@ impl sdl_data {
         let new_canvas = new_window.into_canvas().build()?;
         let new_event_pump = new_context.event_pump()?;
 
-        Ok(sdl_data {
+        Ok(SDLData  {
             context: new_context,
             video_subsystem: new_vsubsystem,
             canvas: new_canvas,
@@ -88,16 +93,23 @@ impl sdl_data {
     }
 
     // renders the entire game
-    fn draw_window(&mut self, pdata: &pong_data) {
+    fn draw_window(&mut self, pdata: &PongData) {
         // clear the screen
         self.canvas
             .set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
         self.canvas.clear();
 
-        // render the pong_data
-        self.canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
+        // render the PongData
+        self.canvas
+            .set_draw_color(sdl2::pixels::Color::RGB(255, 255, 255));
         let _ = self.canvas.fill_rect(pdata.player1);
-        
+        let _ = self.canvas.fill_rect(pdata.player2);
+        let _ = self.canvas.fill_rect(pdata.ball);
+
+        // render the board
+        let net: Rect = Rect::new(445, 0, 10, 600);
+        let _ = self.canvas.draw_rect(net);
+
         self.canvas.present();
     }
 }
@@ -105,23 +117,36 @@ impl sdl_data {
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Initializing SDL2...");
 
-    let mut game_data = sdl_data::new()?;
-    let mut pdata = pong_data::new();
+    let mut game_data = SDLData ::new()?;
+    let mut pdata = PongData::new();
 
-    let mut frame_time: Instant;
+    // framerate control structs
+    let mut start_frame_time: Instant;
+    let mut start_second_time: Instant = Instant::now();
+    let mut frame_render_time: Duration;
+    let mut frames: i32 = 0;
 
-    let frame_duration: f32 = 1000f32 / 15f32;
+    const FRAME_TIME: f32 = 1000f32 / 30f32;
+    const ONE_SECOND: Duration = Duration::from_millis(1000);
 
     println!("SDL is done initializing");
 
     'run: loop {
-        frame_time = Instant::now();
+        start_frame_time = Instant::now();
 
         for event in game_data.event_pump.poll_iter() {
             // handle events
             match event {
                 sdl2::event::Event::Quit { .. } => break 'run,
-                sdl2::event::Event::KeyDown { keycode: Some(Keycode::Down), ..  } => pdata.move_player(paddle_direction::Down),
+                sdl2::event::Event::KeyDown {
+                    keycode: Some(Keycode::Down),
+                    ..
+                } => pdata.move_player(PaddleDuration::Down),
+
+                sdl2::event::Event::KeyDown {
+                    keycode: Some(Keycode::Up),
+                    ..
+                } => pdata.move_player(PaddleDuration::Up),
                 _ => {}
             }
         }
@@ -130,16 +155,20 @@ fn main() -> Result<(), Box<dyn Error>> {
         pdata.simulate();
 
         // frame control
+        frame_render_time = Instant::now() - start_frame_time;
 
-        // TODO
-        let duration = frame_time.elapsed().as_nanos();
+        if FRAME_TIME > frame_render_time.as_millis() as f32 {
+            frames += 1;
 
-        if frame_duration > duration as f32 {
-            let wait_time_millis = frame_duration - duration as f32;
+            let sleep_time: Duration = Duration::from_millis((FRAME_TIME as u128 - frame_render_time.as_millis()) as u64);
 
-            let delta_duration = Duration::from_millis(wait_time_millis as u64);
+            std::thread::sleep(sleep_time);
+        }
 
-            std::thread::sleep(delta_duration);
+        if start_second_time.elapsed() >= ONE_SECOND {
+            println!("{}", frames);
+            frames = 0;
+            start_second_time = Instant::now();
         }
 
         // draw
